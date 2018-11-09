ee49941e23cfce3690c2ead5393b34bc0eea361a
Ravi S. Rāmphal <rramphal@gmail.com>
2018-11-09 11:04:24 -0800

    remove unused service worker

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthWrapper from './AuthWrapper';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<AuthWrapper />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
