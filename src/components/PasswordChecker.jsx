a5dfe2dc7fbf62a4c5bb7bebf5da606d3b687931
Ravi S. Rāmphal <rramphal@gmail.com>
2018-11-13 13:07:03 -0800

    revamped login UI

--- src/components/PasswordChecker.jsx
+++ src/components/PasswordChecker.jsx
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import crypto from 'crypto-js';
 import styled from 'styled-components';
-
-import COLORS from '../utils/colors';
+import {
+  Col,
+  Container,
+  Hidden,
+  Row,
+  Visible,
+} from 'react-grid-system';
 
 import BrandLogo from './BrandLogo';
 
-const HASH = '7fb2990d51938b98cd97bbc04cd9027c9e4e0e6d21a258a8d9ac10febebdcac64d63025ed50a6ee6aec6b666eaf2549070fbc2250f5d0f31ab0f15fc9896c6df';
+import HASH from '../utils/hash';
 
-const StyledPasswordChecker = styled.div`
-  height           : 100vh;
-  display          : flex;
-  flex-direction   : row;
-  align-items      : center;
-  justify-content  : center;
-  background-color : ${COLORS.SITE_BACKGROUND};
+const inputStyles = `
+  height        : 40px;
+  width         : 100%;
+  margin-top    : 10px;
+  border-radius : 3px;
+  font-size     : 20px;
+  line-height   : 36px;
+  text-align    : center;
+`;
+
+const StyledInput = styled.input`
+  border-radius: 3px;
+  ${inputStyles}
+`;
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  ${inputStyles}
 `;
 
 class PasswordChecker extends Component {
@@ -29,41 +45,55 @@ class PasswordChecker extends Component {
 
     e.preventDefault();
     e.stopPropagation();
+
     const hashed = crypto.SHA3(plaintext).toString();
     // console.log(hashed, hashed === HASH);
     if (hashed === HASH) return authorized();
     this.setState({ plaintext: '' });
-
     return null;
   }
 
   handleChange = (e) => {
-    this.setState({ plaintext: e.target.value });
+    this.setState({
+      plaintext: e.target.value,
+    });
   }
 
   render() {
     const { plaintext } = this.state;
+
     return (
-      <div className="PasswordChecker">
-        <h1
-          style={{ marginRight: '2vw' }}
-          className="App-header serif f2 underline white"
-        >
-          <a href="http://fast.ai" target="_blank" rel="noopener noreferrer">fast.ai</a>
-        </h1>
-        <form onSubmit={this.checkPass}>
-          <input
-            type="password"
-            value={plaintext}
-            onChange={this.handleChange}
-          />
-          <button
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+      <form
+        onSubmit={this.checkPass}
+      >
+        <Container>
+          {/* center content vertically */}
+          <Row align="center" style={{ height: '100vh' }}>
+            <Col>
+              <Row>
+                <Col xs={12} sm={4} lg={3}>
+                  <Visible xs><BrandLogo /></Visible>
+                  <Hidden xs><BrandLogo right /></Hidden>
+                </Col>
+                <Col xs={12} sm={5}>
+                  <StyledInput
+                    type="password"
+                    value={plaintext}
+                    onChange={this.handleChange}
+                  />
+                </Col>
+                <Col xs={12} sm={3}>
+                  <StyledButton
+                    type="submit"
+                  >
+                    Log In
+                  </StyledButton>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </form>
     );
   }
 }
