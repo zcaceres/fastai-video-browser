531dd0c3127029327d00281ac0336b416443a19f
Jeremy Howard <info@fast.ai>
2019-01-21 17:26:48 -0700

    init youtube replace

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,41 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import lesson1Vid from '../assets/dl-1-1/video.mp4';
-import lesson2Vid from '../assets/dl-1-2/video.mp4';
-import lesson3Vid from '../assets/dl-1-3/video.mp4';
-import lesson4Vid from '../assets/dl-1-4/video.mp4';
-import lesson5Vid from '../assets/dl-1-5/video.mp4';
-import lesson6Vid from '../assets/dl-1-6/video.mp4';
-import lesson7Vid from '../assets/dl-1-7/video.mp4';
+import ReactPlayer from 'react-player'
 
 const VIDEO_SOURCES = [
-  lesson1Vid,
-  lesson2Vid,
-  lesson3Vid,
-  lesson4Vid,
-  lesson5Vid,
-  lesson6Vid,
-  lesson7Vid,
+  "XfoYk_Z5AkI",
+  "ccMHJeQU4Qw",
+  "MpZxV6DVsmM",
+  "9YK6AnqpuEA",
+  "CJKnDu2dxOE",
+  "hkBa9pU-H48",
+  "DGdRC4h78_o",
 ];
 
 const VideoPlayer = (props) => {
+  // "https://www.youtube.com/embed/XfoYk_Z5AkI",
   const { lesson } = props;
 
-  return (
-    <div className="VideoPlayer">
-      <video
-        id={lesson}
-        key={lesson}
-        // We must include key here so video player is re-rendered on props change
-        controls
-        width="100%"
-        height="100%"
-      >
-        <source key={lesson} src={VIDEO_SOURCES[lesson]} type="video/mp4" />
-        <track kind="captions" />
-      </video>
-    </div>
-  );
+  return <ReactPlayer url={`https://www.youtube.com/embed/${VIDEO_SOURCES[lesson]}`} playing />
+  // return (
+  //   <div className="VideoPlayer">
+  //     <video
+  //       id={lesson}
+  //       key={lesson}
+  //       // We must include key here so video player is re-rendered on props change
+  //       controls
+  //       width="100%"
+  //       height="100%"
+  //     >
+  //       <source key={lesson} src={VIDEO_SOURCES[lesson]} type="video/mp4" />
+  //       <track kind="captions" />
+  //     </video>
+  //   </div>
+  // );
 };
 
 VideoPlayer.propTypes = {
